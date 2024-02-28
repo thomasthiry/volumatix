13f3e2d02cf56d458a475f5538fb4c1fc386197f
Thomas Thiry <thomas.thiry.external@worldline.com>
2024-02-28 22:05:45 +0100

    2 hard coded presets

--- Evolve11.Volumatix/MainPage.xaml.cs
+++ Evolve11.Volumatix/MainPage.xaml.cs
@@ -1,11 +1,20 @@
-﻿using System.Reactive.Linq;
-using System.Xml.Linq;
-using Blu4Net;
-
-namespace Evolve11.Volumatix
+﻿namespace Evolve11.Volumatix
 {
+    public class PresetManager
+    {
+        public static void Apply(Preset preset)
+        {
+            using var client = new HttpClient();
+
+            client.GetAsync($"http://{preset.Host}/Volume?level={preset.Volume}&tell_slaves=1").GetAwaiter().GetResult();
+        }
+    }
+
     public partial class MainPage : ContentPage
     {
+        private Preset Preset1 = new ("192.168.129.1:11000", 20);
+        private Preset Preset2 = new ("192.168.129.1:11000", 15);
+
         public MainPage()
         {
             InitializeComponent();
@@ -13,11 +22,24 @@ namespace Evolve11.Volumatix
 
         private void OnSetVolume1Clicked(object sender, EventArgs e)
         {
-            var url = "http://192.168.129.1:11000/Volume?level=20&tell_slaves=1";
-            using var client = new HttpClient();
+            PresetManager.Apply(Preset1);
+        }
 
-            client.GetAsync(url).GetAwaiter().GetResult();
+        private void OnSetVolume2Clicked(object? sender, EventArgs e)
+        {
+            PresetManager.Apply(Preset2);
         }
     }
 
+    public class Preset
+    {
+        public Preset(string host, int volume)
+        {
+            Host = host;
+            Volume = volume;
+        }
+
+        public string Host { get; }
+        public int Volume { get; }
+    }
 }
