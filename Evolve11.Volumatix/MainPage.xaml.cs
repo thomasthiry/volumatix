5082192f5a3113f95e5c27f41ec533b4d2f697ed
Thomas Thiry <thomas.thiry.external@worldline.com>
2024-02-28 22:25:23 +0100

    Introduce PresetManager

--- Evolve11.Volumatix/MainPage.xaml.cs
+++ Evolve11.Volumatix/MainPage.xaml.cs
@@ -2,18 +2,29 @@
 {
     public class PresetManager
     {
-        public static void Apply(Preset preset)
+        public static void Apply(DevicePreset devicePreset)
         {
             using var client = new HttpClient();
 
-            client.GetAsync($"http://{preset.Host}/Volume?level={preset.Volume}&tell_slaves=1").GetAwaiter().GetResult();
+            client.GetAsync($"http://{devicePreset.Host}/Volume?level={devicePreset.Volume}&tell_slaves=0").GetAwaiter().GetResult();
+        }
+
+        public static void Apply(Preset preset)
+        {
+            foreach (var devicePreset in preset.DevicePresets)
+            {
+                Apply(devicePreset);
+            }
         }
     }
 
     public partial class MainPage : ContentPage
     {
-        private Preset Preset1 = new ("192.168.129.1:11000", 20);
-        private Preset Preset2 = new ("192.168.129.1:11000", 15);
+        private static DevicePreset DevicePreset1 = new ("192.168.129.1:11000", 20); // Salon
+        private static DevicePreset DevicePreset2 = new ("192.168.129.1:11000", 15);
+        private static DevicePreset DevicePreset3 = new ("192.168.129.4:11000", 15); // Cuisine gauche
+
+        private Preset preset1 = new(new List<DevicePreset> { DevicePreset1 });
 
         public MainPage()
         {
@@ -22,18 +33,28 @@
 
         private void OnSetVolume1Clicked(object sender, EventArgs e)
         {
-            PresetManager.Apply(Preset1);
+            PresetManager.Apply(DevicePreset1);
         }
 
         private void OnSetVolume2Clicked(object? sender, EventArgs e)
         {
-            PresetManager.Apply(Preset2);
+            PresetManager.Apply(preset1);
         }
     }
 
     public class Preset
     {
-        public Preset(string host, int volume)
+        public List<DevicePreset> DevicePresets { get; }
+
+        public Preset(List<DevicePreset> devicePresets)
+        {
+            DevicePresets = devicePresets;
+        }
+    }
+
+    public class DevicePreset
+    {
+        public DevicePreset(string host, int volume)
         {
             Host = host;
             Volume = volume;
